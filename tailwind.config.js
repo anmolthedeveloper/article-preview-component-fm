80c22eb667f834a49af8d861db64c25257ecc286
Anmol Singh Tuteja <anmolthedeveloper@gmail.com>
2023-06-11 19:03:12 +0530

    add font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}", "./images/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        desaturatedDarkBlue: "hsl(214, 17%, 51%)",
+        grayishBlue: "hsl(212, 23%, 69%)",
+        lightGrayishBlue: "hsl(210, 46%, 95%)",
+      },
+      fontFamily: {
+        manrope: ["Manrope"],
+      },
+    },
   },
   plugins: [],
 };
